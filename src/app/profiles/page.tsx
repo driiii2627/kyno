69d7683a8e7031075ee40b485c8b55025b3b60b9
Kyno Builder <kyno@project.com>
2025-12-27 14:10:46 -0300

    123

--- src/app/profiles/page.tsx
+++ src/app/profiles/page.tsx
@@ -13,29 +13,46 @@ interface Profile {
     avatar_url: string;
 }
 
-const AVATARS = [
-    // Famous Characters (TMDB High Res)
-    'https://image.tmdb.org/t/p/w500/5weKu49pzJCt06OPpjvT80efnQj.jpg', // No Way Home Spider-Man
-    'https://image.tmdb.org/t/p/w500/udDclJoHjfjb8JzDKgdHg2kT8pj.jpg', // Joker
-    'https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg', // Dark Knight Batman
-    'https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg', // Fight Club
-    'https://image.tmdb.org/t/p/w500/8t882GWfgO72787x8j0TfC2d38.jpg', // Pulp Fiction (Mia)
-    'https://image.tmdb.org/t/p/w500/saPqeb5rC7PQJv7g12r0sH2C0p.jpg', // Avengers (Iron Manish art)
-
-    // Vibrant Avatars
-    'https://api.dicebear.com/7.x/notionists/svg?seed=Felix',
-    'https://api.dicebear.com/7.x/notionists/svg?seed=Aneka',
-    'https://api.dicebear.com/7.x/adventurer/svg?seed=Zoey',
-    'https://api.dicebear.com/7.x/adventurer/svg?seed=Jack',
-    'https://api.dicebear.com/7.x/micah/svg?seed=Milo',
-    'https://api.dicebear.com/7.x/micah/svg?seed=Lola',
-
-    // Classic
-    'https://api.dicebear.com/7.x/avataaars/svg?seed=Rocky',
-    'https://api.dicebear.com/7.x/avataaars/svg?seed=Luna',
-    'https://api.dicebear.com/7.x/bottts/svg?seed=Robot1',
-    'https://api.dicebear.com/7.x/bottts/svg?seed=Robot2'
-];
+const AVATAR_CATEGORIES = {
+    'Stranger Things': [
+        'https://i.ibb.co/N6x3VF66/AAAABSo-jd-QDT6-VMJr7-Va-Lx-XSAWia-Dwya15-Zpqe7r5-z-Iwc8-Y70d2p3a-RC-r-Kk-LCxgak-AKf-lj-VW1-Uma-fx-R06es.png',
+        'https://i.ibb.co/ndBXZMf/AAAABULyt-B835457fm5-Y0-T4-E52fuj-CDj-IMKzkqrk-T9v-UXg-Avxq-LXz26t-Ct-DJGG5-KMCpe-UFc-IJc-BDh-Pl-VKaws-Z.png',
+        'https://i.ibb.co/0jQPBLHs/AAAABby-CFMx-Hi-QFn-Flr-HYnwm-Jcam-LTSWqz-M8-W-r1t-Sg1-NL51m-Cu8v-m-GIJLmdj-N3g-W-6-SN3-Fi8l-Yl-Xl-ZHY83.png',
+        'https://i.ibb.co/ycvdGVDM/AAAABZ1-Xom-GA7-Sa-DOQ4w-Xc-U-Jky-Hq-Zjz-PBZHj-D57k-Lt-Uw-YOKt-Ym8-MXX7-OU54kg-z-QT-f-RWv-UDj3-QTq5-ZVXf-S.png'
+    ],
+    'Lucifer': [
+        'https://i.ibb.co/dwNB3356/AAAABZTO6i-Ld-MK66-Sb-Fd-LYOsu-WNQSy-Ua-FD8-GPNp-Auhn2-U7s-Ufv95jj5-Kb7-Gqr6-CSayp-CIj-Oi7ldgq-JPejm-XVud.png',
+        'https://i.ibb.co/gFMWK0k1/AAAABW75gw1fd6-E-71-Wu7xy-PYjr-Wov-Pjv-Fy-Qt8e1x-Qy44c-Rj-Yqd-TVWc947w-ZWX089gg-XDRzxg-a-Ubb-Y8-Bo-H2-TA.png',
+        'https://i.ibb.co/s9jYxdSw/AAAABTRrsz-1ts-M-2m-bz-EY8s-VHp3-NZKYjuf-MMy-X8p63x-MM5a-Nza3kluw3-ccux-MNPJ5ft-TOv-Nczlx-D094-Www-Div.png'
+    ],
+    'The Witcher': [
+        'https://i.ibb.co/4w14nd6F/AAAABe-QVP3-Hvz-LKP1d-E6-BP5-BAkvs4-f-JKzj5p-IAyo-B4-Fld25dl-DAAQ-MW8-Wy2-O-L2qs-U5l-CENVQjr-LRFQmcyam-Pgn.png',
+        'https://i.ibb.co/NndGNKM0/AAAABb8b7-p43y-Jw3j-AQF6p-TNc4-F0rkv-BXb-LZAKZsc-Usw-VI-X2-Avtjdox-BSMyw-Hk-FQmv9-MIhe-m-Hy-IJxghqj-M5-S.png',
+        'https://i.ibb.co/1YZ0bWQs/AAAABd-GOg-Or-DX96v-Yc-S8x-UKBfaf-HTDP2-Qx-Ohl-IIar-O3le-G8-Tl-KFQ9-Vlmn6mz-AJJQBOhcj5-UL8f-Tz-Kz-TTPS2.png'
+    ],
+    'Outer Banks': [
+        'https://i.ibb.co/LdjFTSF9/AAAABb-CTHBMt-KGXNw6u-U1lip-XZii9-N8-SAdql-Gsfj-UUO6bn-Sda152-Fbr1m25k-G6a-Iqcng-Hk-LRk-ODT6b8g2u-Hx9o.png',
+        'https://i.ibb.co/H9gxvhx/AAAABckq-Qwm-ATm-FM6-XMGph7d-Lk-Qg0i1r0h-Ak8h-Wam5-RFAJDx-N05ua-QHQh-Xo1z5-B-t-Sgxu4-UZRIFv-Gju-Ko-Mb4j.png',
+        'https://i.ibb.co/3Yd1hqHT/AAAABQb-L2f20m-GC4-Y859-IVbe-JCy-WZFe-Pl-Gc-BKlzbe-WLRFwz-Ul76ci-P1p-OO8j-KAXi-PWu-BPHepb-V2b-sk-L84z.png',
+        'https://i.ibb.co/C5fQ9Z68/AAAABc5zk4i-Uqh-CDL4k-N0i-H3n-Zjyg-jzv-KNBib-MQsv0-Nb-ZR2qn-O46-Esg-Vs-MP8-ITwvt-Na-BMWTfnse-YEIss-Ii7-D.png'
+    ],
+    'La Casa de Papel': [
+        'https://i.ibb.co/nqPfS4d6/AAAABVrl86-Z63fibn4z-JIZE1g-W3-JGr-ric40bd-TPDk3j-GUSS-Nb-Zwb6b-Uo-Gj-He-D3k-HMREUPTtcy-Hum-Ui-MDVp13y.png',
+        'https://i.ibb.co/678mhcFR/AAAABZbs-Wc-BAKful-E6f-F1-Lmh-El1fr7x-Kkj-Dnoa-TCE0-Tg-Y4-PMr22-Sswku-Ljgaya-GKm-A740if-Ua-BA63-T-FF-6-Ca.png'
+    ],
+    'Original Kyno+': [
+        'https://api.dicebear.com/7.x/notionists/svg?seed=Felix',
+        'https://api.dicebear.com/7.x/notionists/svg?seed=Aneka',
+        'https://api.dicebear.com/7.x/adventurer/svg?seed=Zoey',
+        'https://api.dicebear.com/7.x/adventurer/svg?seed=Jack',
+        'https://api.dicebear.com/7.x/micah/svg?seed=Milo',
+        'https://api.dicebear.com/7.x/micah/svg?seed=Lola',
+        'https://api.dicebear.com/7.x/avataaars/svg?seed=Rocky',
+        'https://api.dicebear.com/7.x/avataaars/svg?seed=Luna',
+        'https://api.dicebear.com/7.x/bottts/svg?seed=Robot1',
+        'https://api.dicebear.com/7.x/bottts/svg?seed=Robot2'
+    ]
+};
 
 export default function ProfilesPage() {
     const router = useRouter();
@@ -57,7 +74,7 @@ export default function ProfilesPage() {
     // Form State
     const [editProfile, setEditProfile] = useState<Profile | null>(null);
     const [newName, setNewName] = useState('');
-    const [newAvatar, setNewAvatar] = useState(AVATARS[0]);
+    const [newAvatar, setNewAvatar] = useState(AVATAR_CATEGORIES['Original Kyno+'][0]);
     const [showAvatarPicker, setShowAvatarPicker] = useState(false);
 
     // Auth/Status
@@ -251,17 +268,24 @@ export default function ProfilesPage() {
                                 <h2 style={{ fontSize: '2rem', margin: 0, fontWeight: 500 }}>Escolher Avatar</h2>
                             </div>
                         </div>
-                        <div className={styles.pickerGrid}>
-                            {AVATARS.map((av, idx) => (
-                                <img
-                                    key={idx}
-                                    src={av}
-                                    className={styles.pickerItem}
-                                    onClick={() => {
-                                        setNewAvatar(av);
-                                        setShowAvatarPicker(false);
-                                    }}
-                                />
+                        <div className={styles.pickerScrollContent}>
+                            {Object.entries(AVATAR_CATEGORIES).map(([category, urls]) => (
+                                <div key={category} className={styles.categorySection}>
+                                    <h3 className={styles.categoryTitle}>{category}</h3>
+                                    <div className={styles.pickerGrid}>
+                                        {urls.map((av, idx) => (
+                                            <img
+                                                key={idx}
+                                                src={av}
+                                                className={styles.pickerItem}
+                                                onClick={() => {
+                                                    setNewAvatar(av);
+                                                    setShowAvatarPicker(false);
+                                                }}
+                                            />
+                                        ))}
+                                    </div>
+                                </div>
                             ))}
                         </div>
                     </div>
