4e98696a53d3c217212efab0e77d4e470cd321c7
Kyno Builder <kyno@project.com>
2025-12-29 22:16:09 -0300

    Fix: Simplify Ambient Background (Black + Static Subtle Blue Gradient)

--- web/src/components/layout/AmbientBackground.tsx
+++ web/src/components/layout/AmbientBackground.tsx
@@ -5,11 +5,7 @@ import styles from './AmbientBackground.module.css';
 export default function AmbientBackground() {
     return (
         <div className={styles.container}>
-            {/* The Moving Light Source */}
-            <div className={styles.blob} />
-
-            {/* The Glass/Texture Overlay */}
-            <div className={styles.glassOverlay} />
+            <div className={styles.gradient} />
         </div>
     );
 }
